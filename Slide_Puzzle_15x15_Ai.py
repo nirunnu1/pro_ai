3b40a12e83f4f64f7a9060da8b51c695d2b3c7d7
Nirun Tulayote <nirunnu1@hotmail.com>
2016-05-09 18:42:58 +0700

    Update Slide_Puzzle_15x15_Ai.py

--- Slide_Puzzle_15x15_Ai.py
+++ Slide_Puzzle_15x15_Ai.py
@@ -1,12 +1,7 @@
-# Slide Puzzle
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
 import pygame, sys, random
 from pygame.locals import *
 
-# Create the constants (go ahead and experiment with different values)
+
 BOARDWIDTH = 4  # number of columns in the board
 BOARDHEIGHT = 4 # number of rows in the board
 TILESIZE = 80
